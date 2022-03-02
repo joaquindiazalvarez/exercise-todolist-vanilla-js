8dfddb62c970250d5e047176feb727fbe447c7ae
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-03-02 00:01:39 +0000

    first working version

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let liParent = document.querySelector("ul");
+let allSpans = document.querySelectorAll("li span");
+for (let i = 0; i < allSpans.length; i++) {
+  allSpans[i].addEventListener("click", () => {
+    allSpans[i].parentNode.parentNode.removeChild(allSpans[i].parentNode);
+  });
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let addtodo = document.querySelector("#addToDo");
+  addtodo.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+      let create = document.createElement("li");
+      create.innerHTML =
+        '<span><i class="fa-solid fa-xmark me-3"></i></span>' + addtodo.value;
+      liParent.appendChild(create);
+      let span = create.firstChild;
+      span.addEventListener("click", function() {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+      });
+      addtodo.value = "";
+    }
+  });
 };
